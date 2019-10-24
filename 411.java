1181a801e20d2593323a9b4f198251c32fceece6
guideon16 <55553676+guideon16@users.noreply.github.com>
2019-10-24 19:56:18 +0200

    Create 411.java

---
+++ 411.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class P411_SobreTelaAraña {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int casos = sc.nextInt();
+		
+		int numElefantes = sc.nextInt();
+		int pesoElefante = sc.nextInt();
+		
+		int pesotela = 0;
+		
+		while (true) {
+			
+			while (numElefantes == 0) {
+				
+				if (numElefantes<pesoElefante) {
+					numElefantes++;
+				}
+				else {
+			break;
+	
+			}
+			
+		}
+		
+			
+}
+	}}
